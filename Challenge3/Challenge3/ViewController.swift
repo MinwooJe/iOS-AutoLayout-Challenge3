b93d9eabbe4d211523117837f36d3df0a37845af
MinwooJe <gpatration77@gmail.com>
2023-10-27 18:02:22 +0900

    feat: Login View 구현

--- Challenge3/Challenge3/ViewController.swift
+++ Challenge3/Challenge3/ViewController.swift
@@ -9,9 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var buttons: [UIButton]!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(adjustButtonDynamicType), name: UIContentSizeCategory.didChangeNotification, object: nil)
+    }
+    
+    @objc func adjustButtonDynamicType() {
+        buttons.forEach { (button) in
+            button.titleLabel?.adjustsFontSizeToFitWidth = true
+        }
     }
 
 
